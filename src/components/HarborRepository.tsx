e780cfd0108b1356d447302a4b356cf64008ef38
Brett Wright <wright.brett@gmail.com>
2021-07-05 20:38:01 +0200

    refacted code and fix dark mode theme

--- src/components/HarborRepository.tsx
+++ src/components/HarborRepository.tsx
@@ -1,132 +1,165 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { columns } from "./tableHeadings";
 import ReactSpeedometer from "react-d3-speedometer";
 import { Card, CardActions, Button } from "@material-ui/core";
+import { getTheme } from "./theme";
 
-class HarborRepository extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
+function HarborRepository(props: RepositoryProps ){
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [ repository, setRepository] = useState<Repository[]>([])
 
-    this.state = {
-      isLoaded: false,
-      data: null,
-      repoUrl: "",
-    };
-  }
+  useEffect(() => {
+    setLoading(false)
 
-  async componentDidMount() {
-    let backendUrl = window.location.origin;
-    if (backendUrl.includes("3000")) {
-      backendUrl = backendUrl.replace("3000", "7000");
-    }
-
-    const response = await fetch(
-      `${backendUrl}/api/harbor/artifacts?project=${this.props.project}&repository=${this.props.repository}`
-    ).then((res) => res.json());
-
-    this.setState({
-      isLoaded: true,
-      data: response,
-      repoUrl: response?.[0].repoUrl,
-    });
-  }
-
-  render() {
-    const { isLoaded, data, repoUrl } = this.state;
-
-    if (!isLoaded) {
-      return <p>Loading...</p>;
-    }
-
-    if (this.props.widget) {
-      let severityNumber: number = 0;
-      const severityText: string = data?.[0].vulnerabilities.severity;
-      switch (severityText) {
-        case "Low":
-          severityNumber = 150;
-          break;
-
-        case "Medium":
-          severityNumber = 250;
-          break;
-
-        case "High":
-          severityNumber = 350;
-          break;
-
-        case "Critical":
-          severityNumber = 450;
-          break;
-
-        default:
-          severityNumber = 50;
-          break;
+    async function getRepository() {
+      let backendUrl = window.location.origin;
+      if (backendUrl.includes("3000")) {
+        backendUrl = backendUrl.replace("3000", "7000");
       }
+      const response = await fetch(`${backendUrl}/api/harbor/artifacts?project=${props.project}&repository=${props.repository}`)
+      const json = await response.json()
+    
+      if (json.hasOwnProperty("error")) {
+        setError(true);
+        setErrorMsg(json.error.message);
+      }
+      setRepository(json);
 
-      return (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <ReactSpeedometer
-            value={severityNumber}
-            width={450}
-            minValue={0}
-            maxValue={500}
-            segmentColors={[
-              "#6ad72d",
-              "#ade228",
-              "#ecdb23",
-              "#f6961e",
-              "#ff471a",
-            ]}
-            customSegmentStops={[0, 100, 200, 300, 400, 500]}
-            currentValueText="vulnerability levels"
-            customSegmentLabels={[
-              {
-                text: "None",
-              },
-              {
-                text: "Low",
-              },
-              {
-                text: "Medium",
-              },
-              {
-                text: "High",
-              },
-              {
-                text: "Critical",
-              },
-            ]}
-          />
-        </div>
-      );
     }
+    getRepository()
+
+    setTimeout(() => {
+      setLoading(true);
+    }, 3000);
+
+  },[props.project, props.repository])
+
+  const theme = getTheme();
+
+  if (!loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <p>{errorMsg}</p>;
+  }
+
+
+  if (props.widget) {
+          let severityNumber: number = 0;
+          const severityText: string = repository?.[0].vulnerabilities.severity;
+          switch (severityText) {
+            case "Low":
+              severityNumber = 150;
+              break;
+    
+            case "Medium":
+              severityNumber = 250;
+              break;
+    
+            case "High":
+              severityNumber = 350;
+              break;
+    
+            case "Critical":
+              severityNumber = 450;
+              break;
+    
+            default:
+              severityNumber = 50;
+              break;
+          }
+    
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <ReactSpeedometer
+                value={severityNumber}
+                width={450}
+                minValue={0}
+                maxValue={500}
+                segmentColors={[
+                  "#6ad72d",
+                  "#ade228",
+                  "#ecdb23",
+                  "#f6961e",
+                  "#ff471a",
+                ]}
+                customSegmentStops={[0, 100, 200, 300, 400, 500]}
+                currentValueText="vulnerability levels"
+                customSegmentLabels={[
+                  {
+                    text: "None",
+                  },
+                  {
+                    text: "Low",
+                  },
+                  {
+                    text: "Medium",
+                  },
+                  {
+                    text: "High",
+                  },
+                  {
+                    text: "Critical",
+                  },
+                ]}
+              />
+            </div>
+          );
+        }
+    
+
     return (
       <div>
         <Card>
           <CardActions>
             <Button size="small">
-              <a href={repoUrl}>Learn More</a>
+              <a href={repository[0].repoUrl}>Learn More</a>
             </Button>
           </CardActions>
         </Card>
         <DataTable
+          theme={theme}
           striped
           title="Docker Images"
           columns={columns}
-          data={data}
+          data={repository}
           defaultSortField="pushTime"
           defaultSortAsc={false}
         />
       </div>
     );
-  }
 }
+
+interface RepositoryProps {
+  widget: any;
+  project: string,
+  repository: string,
+}
+
+
+interface Repository {
+  size: number
+  tag: string
+  pullTime: string
+  pushTime: string
+  projectID: number
+  repoUrl: string
+  vulnerabilities: Vulnerabilities
+}
+
+interface Vulnerabilities {
+  count: number
+  severity: string
+}
+
 
 export { HarborRepository };
---
+++ src/components/theme.ts
@@ -0,0 +1,10 @@
+export function getTheme() {
+    const localTheme = window.localStorage.getItem("theme");
+    let theme: string = "light";
+    if (localTheme === "dark") {
+      theme = localTheme;
+    }
+  
+    return theme;
+  }
+  
