d960a354856b5ab2d899ca6fa45edff4e4aadbf6
Brett Wright <wright.brett@gmail.com>
2021-07-05 21:23:08 +0200

    fixed up entity usage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Entity } from "@backstage/catalog-model";
+import { useEntity } from "@backstage/plugin-catalog-react";
 import { MissingAnnotationEmptyState } from "@backstage/core";
 import React from "react";
 import { Route, Routes } from "react-router";
@@ -6,11 +6,13 @@ import { HarborDashboardPage } from "./components/HarborDashboardPage";
 import { HARBOR_ANNOTATION_REPOSITORY } from "./components/useHarborAppData";
 import { isHarborAvailable } from "./plugin";
 
-export const Router = ({ entity }: { entity: Entity }) =>
-  !isHarborAvailable(entity) ? (
+export const Router = () => {
+  const { entity } = useEntity();
+  return !isHarborAvailable(entity) ? (
     <MissingAnnotationEmptyState annotation={HARBOR_ANNOTATION_REPOSITORY} />
   ) : (
     <Routes>
       <Route path="/" element={<HarborDashboardPage entity={entity} />} />
     </Routes>
   );
+};
