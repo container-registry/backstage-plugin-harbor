cdae974aa778ccafffed8ce8ab1ae11a57a60c05
Brett Wright <wright.brett@gmail.com>
2021-07-06 12:48:16 +0200

    yarn prettier -w ./src

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export { harborPlugin as plugin, isHarborAvailable, EntityHarborCard, EntityHarborContent } from "./plugin";
+export {
+  harborPlugin as plugin,
+  isHarborAvailable,
+  EntityHarborCard,
+  EntityHarborContent,
+} from "./plugin";
 export * from "./api";
 export * from "./Router";
 export { HarborWidget } from "./components/HarborWidget";
