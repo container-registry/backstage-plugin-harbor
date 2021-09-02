2df3551c5fedd83234786fc8cefcc5b618f09593
Andrei Predoiu <andrei.predoiu@bestseller.com>
2021-09-02 10:19:58 +0200

    Updated entity handling

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,24 @@
-import { Entity } from "@backstage/catalog-model";
+import React from 'react';
+import { Entity } from '@backstage/catalog-model';
+import { useEntity } from '@backstage/plugin-catalog-react';
+import { Route } from 'react-router';
+import { FlatRoutes } from '@backstage/core-app-api';
 import { MissingAnnotationEmptyState } from "@backstage/core-components";
-import React from "react";
-import { Route, Routes } from "react-router";
 import { HarborDashboardPage } from "./components/HarborDashboardPage";
 import { HARBOR_ANNOTATION_REPOSITORY } from "./components/useHarborAppData";
 import { isHarborAvailable } from "./plugin";
 
-export const Router = ({ entity }: { entity: Entity }) => {
+type Props = {
+  /** @deprecated The entity is now grabbed from context instead */
+  entity?: Entity;
+};
+export const Router = (_props: Props) => {
+  const { entity } = useEntity();
   return !isHarborAvailable(entity) ? (
     <MissingAnnotationEmptyState annotation={HARBOR_ANNOTATION_REPOSITORY} />
   ) : (
-    <Routes>
+    <FlatRoutes>
       <Route path="/" element={<HarborDashboardPage entity={entity} />} />
-    </Routes>
+    </FlatRoutes>
   );
 };
