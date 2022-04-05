956dcd2d06e0e7c915382adac7cd13d225c8427b
Brett Wright <brett.wright@bestseller.com>
2022-04-05 12:02:08 +0200

    added dev step

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { createDevApp } from '@backstage/dev-utils';
-import { harborPlugin, EntityHarborContent } from '../src/plugin';
+import { Entity } from '@backstage/catalog-model'
+import { createDevApp } from '@backstage/dev-utils'
+import { EntityProvider } from '@backstage/plugin-catalog-react'
+import React from 'react'
+import { HarborPage, harborPlugin, HarborWidget } from '../src/plugin'
+
+const mockEntity: Entity = {
+  apiVersion: 'backstage.io/v1alpha1',
+  kind: 'Component',
+  metadata: {
+    name: 'mock-service',
+    description: 'this is a mock service',
+    annotations: {
+      'goharbor.io/repository-slug': 'project/repo',
+    },
+  },
+  spec: {
+    lifecycle: 'production',
+    type: 'service',
+    owner: 'user:guest',
+  },
+}
 
 createDevApp()
   .registerPlugin(harborPlugin)
   .addPage({
-    element: <EntityHarborContent />,
+    element: (
+      <EntityProvider entity={mockEntity}>
+        <HarborPage />
+        <HarborWidget />
+      </EntityProvider>
+    ),
     title: 'Root Page',
-    path: '/harbor'
+    path: '/backstage-plugin-harbor',
   })
-  .render();
+  .render()
